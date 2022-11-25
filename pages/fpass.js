40552c7bcf6f6ecc9e53de0c822840bd73ff52c1
Gelosu <118784451+Gelosu@users.noreply.github.com>
2022-11-25 15:16:11 +0800

    Add files via upload

---
+++ pages/fpass.js
@@ -0,0 +1,12 @@
+import Fpass from "../components/fpass";
+
+export default function fpass() {
+    return (
+      
+      <div>
+        <Fpass/>
+        
+      </div>
+      
+    );
+  }
---
+++ pages/login.js
@@ -0,0 +1,10 @@
+import Login from "../components/login";
+
+export default function login() {
+    return (
+
+      <div>
+        <Login/>
+      </div>
+    );
+  }
