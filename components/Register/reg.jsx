c439fd1012ba33c716eb4c0042301126c5f0e702
Gilooooo <118784503+Gilooooo@users.noreply.github.com>
2022-11-25 17:02:13 +0800

    Add files via upload

---
+++ components/Register/reg.jsx
@@ -0,0 +1,24 @@
+import styles from "./reg.module.css";
+
+export default function REG (){
+    return(
+        <div className={styles.container}>
+            <div className={styles.LEFT}>
+                <h1>SIGN UP</h1>
+            </div>
+
+            <div className={styles.RIGHT}>
+                <h6>Username</h6>
+                <input type="text"/>
+                <h6>Email</h6>
+                <input type="text"/>
+                <h6>Password</h6>
+                <input type="text"/>
+                <h6>Confirm Password</h6>
+                <input type="text"/>
+                <br></br>
+                <button id="btn">SIGN UP</button>
+            </div>
+        </div>
+    )
+}
