012e31433cf765085dae6ca16d22dd0913b7a175
Gelosu <118784451+Gelosu@users.noreply.github.com>
2022-11-26 15:43:44 +0800

    Update concern.jsx

--- components/Main/concern.jsx
+++ components/Main/concern.jsx
@@ -1,14 +1,13 @@
 import styles from "./concern.module.css";
 
-export default function concern(){
+export default function Concern(){
     return(
         <div className={styles.container}>
             <div >
-                <h5>IF YOU HAVE ANY CONCERNS, 
-                JUST LEAVE A MESSAGE HERE</h5>
+                <h5>IF YOU HAVE ANY CONCERNS, JUST LEAVE A MESSAGE HERE</h5>
             </div>
             <div className={styles.Input}>
-                <input type={styles.input} placeholder="message of user"></input><br/>
+                <input type={styles.input} ></input><br/>
                 <button>SEND</button>
             </div>
         </div>
