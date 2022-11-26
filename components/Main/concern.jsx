a5cb513dc21c09a86cc2abcc2e8d642866812f52
Gilooooo <118784503+Gilooooo@users.noreply.github.com>
2022-11-26 13:24:15 +0800

    Add files via upload

---
+++ components/Main/concern.jsx
@@ -0,0 +1,16 @@
+import styles from "./concern.module.css";
+
+export default function concern(){
+    return(
+        <div className={styles.container}>
+            <div >
+                <h5>IF YOU HAVE ANY CONCERNS, 
+                JUST LEAVE A MESSAGE HERE</h5>
+            </div>
+            <div className={styles.Input}>
+                <input type={styles.input} placeholder="message of user"></input>
+                <button>SEND</button>
+            </div>
+        </div>
+    )
+}
