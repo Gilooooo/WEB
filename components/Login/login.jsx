9501a3cbc60609792ff7a7c159b788e36bdee675
Gelosu <118784451+Gelosu@users.noreply.github.com>
2022-11-26 17:13:41 +0800

    Update login.jsx

--- components/Login/login.jsx
+++ components/Login/login.jsx
@@ -21,27 +21,33 @@ export default function LOGIN() {
         </div>
     
         <div className ={styles.TEXT}>
+            <a href="http://localhost:3000/reg">
         <button>CREATE ACCOUNT</button>
+        </a>
         <a href="http://localhost:3000/fpass"> 
         <button>FORGOT PASSWORD</button>
         </a>
         </div>
+        <hr></hr>
+        <div className={styles.google}>
+        <a href="https://myaccount.google.com/?utm_source=sign_in_no_continue&pli=1">
+        <button>
+        <img src="google.png"></img>
+        </button>
+        </a>
+        <a href="https://www.facebook.com">
+        <button>
+        <img src="facebook.png"></img>
+        </button>
+        </a>
+        <a href="https://appleid.apple.com/sign-in">
+        <button>
+        <img src="apple.png"></img>
+        </button>
+        </a>
         </div>
-        
-        
-
-        
-
-        
-
-      
-
-       
-
-
-
-
-
+        </div>
+    
 
     );
 
