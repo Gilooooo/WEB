0b5143c8968fc21c94a1fb72dcabaf94bb4f84bf
Gelosu <118784451+Gelosu@users.noreply.github.com>
2022-11-27 11:57:45 +0800

    Update fpass.jsx

--- components/Login/fpass.jsx
+++ components/Login/fpass.jsx
@@ -1,10 +1,44 @@
 import styles from "./fpass.module.css";
+import swal from "sweetalert";
+import Swal from "sweetalert2";
 
 
 export default function Fpass() {
-    
+
+   const Clickme =() => {
+        Swal.fire({
+            icon:"success",
+            title: "SUCCESSFUL!",
+            text:"Credentials have been updated",
+            showConfirmButton: true
+        });
+    }
+   
+        const sendemail =() => {
+            swal({
+                icon:"success",
+                title: "SUCCESSFUL!",
+                text:"Code sent on email",
+                showConfirmButton: true
+        });
+        
+
+   }
+    const sendcode =() => {
+    swal({
+        icon:"success",
+        title: "SUCCESSFUL!",
+        text:"Code Correct",
+        showConfirmButton: true
+});
+
+
+}
+
+   
+
     return(
-       
+        
         <div className={styles.container}>
         <div className={styles.image}>
         <a href="http://localhost:3000/WEB">
@@ -17,10 +51,12 @@ export default function Fpass() {
         <div className={styles.pass}>
         <h2>EMAIL</h2>
         <input type="email" />
-        <button>CODE</button>
+        <button onClick={sendemail}>CODE</button>
+
         <h2>CODE</h2>
         <input type="CODE"/>
-        <button>CODE</button>
+        <button onClick={sendcode}>CODE</button>
+       
         </div>
         <div className={styles.newpass}>
         <h2>NEW PASSWORD</h2>
@@ -28,13 +64,19 @@ export default function Fpass() {
         <h2>CONFIRM PASSWORD</h2>\
         <input type="confirm pass"/>
         <p></p>
-        <button>CONFIRM</button>
+    
+        <button onClick={Clickme}>CONFIRM</button>
         </div>
         
+       
+       
+       
+        
+       
 
         </div>
        
        
 
     );
-}
+    }
