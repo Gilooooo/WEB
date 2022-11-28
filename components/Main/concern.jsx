89c200c494f8d14ed2d7ff38b6c2efe66eb5698b
Gelosu <118784451+Gelosu@users.noreply.github.com>
2022-11-29 01:40:12 +0800

    Update concern.jsx

--- components/Main/concern.jsx
+++ components/Main/concern.jsx
@@ -1,14 +1,27 @@
 import styles from "./concern.module.css";
+import swal from "sweetalert";
 
 export default function Concern(){
+    
+    const sent =() => {
+        swal({
+            icon:"success",
+            title: "SUCCESSFUL!",
+            text:"Your Concern have been sent to EOS",
+            showConfirmButton: true
+    });
+}
+    
     return(
-        <div className={styles.container }>
+        
+        <div  className={styles.container}>
             <div >
-                <h5>IF YOU HAVE ANY CONCERNS, JUST LEAVE A MESSAGE HERE</h5>
+                <h5>IF YOU HAVE ANY CONCERNS, 
+                    JUST LEAVE A MESSAGE HERE</h5>
             </div>
             <div className={styles.Input}>
                 <input type={styles.input} ></input><br/>
-                <button>SEND</button>
+                <button onClick={sent}>SEND</button>
             </div>
         </div>
     )
