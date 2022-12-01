2913158a752334c2c19370d0fa2e2b98845abb47
Gelosu <118784451+Gelosu@users.noreply.github.com>
2022-12-01 17:30:02 +0800

    Update login.jsx

--- components/Login/login.jsx
+++ components/Login/login.jsx
@@ -1,26 +1,39 @@
 import styles from "./login.module.css";
 import { useState } from "react";
 import Swal from "sweetalert2";
+import WEB2 from "../../pages/WEB2";
+
 
 export default function LOGIN() {
     const [username,SetUsername] = useState('')
     const [password,SetPassword] = useState('')
-
+   
     const Confirm = () => {
 
         Swal.fire({
-            icon:"info",
-            title: "Welcome User",
+            icon: "success",
+            iconColor:"#31f731",
+            title: "Welcome to  EOS!  " + `${username}` ,
             showCancelButton:true,
-            text:(`${username}`),
-            confirmButtonText:"Confirm",
+            confirmButtonText:"CONFIRM",
             closeOnConfirm: false,
-            closeOnCancel: false
+            closeOnCancel: false,
+            confirmButtonColor: "green",
+            cancelButtonColor: "red",
+            background: "#4d614d",
+            border:"neon green",
+            color:"white",
+            padding:"5px",
+            width:400
            
 
         }).then((result) => {
-            if (result.isConfirmed) {window.location = "http://localhost:3000/WEB2";}
-
+            if (result.isConfirmed) {
+                
+                window.location = "http://localhost:3000/WEB2";} 
+            
+            
+            
         });
 
         
@@ -44,11 +57,13 @@ export default function LOGIN() {
         </div>
         <div className={styles.credit}>
         <h1> </h1>
+        
         <h3> EMAIL / USERNAME</h3>
         <input type="text" id="username" name="username" value={username} onChange={event => SetUsername(event.target.value)}/>
         <h3>PASSWORD</h3>
-        <input type="text" id="password" name="password" value={password} onChange={event => SetPassword(event.target.value)}/>
+        <input type="password" id="password" name="password" value={password} onChange={event => SetPassword(event.target.value)}/>
         <button disabled={!username || !password} onClick={Confirm} >LOGIN</button>
+        
         </div>
     
         <div className ={styles.TEXT}>
