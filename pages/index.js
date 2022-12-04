440a4e4ca0da412bad5ee61d09c8056bc9e50f5f
Gilooooo <118784503+Gilooooo@users.noreply.github.com>
2022-12-04 20:43:11 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 export default function (){
   return(
     <div>
-        Go to http://localhost:3000/WEB
+        <a href="http://localhost:3000/WEB">Go to http://localhost:3000/WEB<a>
     </div>
   )
 }
