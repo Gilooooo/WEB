b26d8fce99de98a8157dd647a2c159b5017100ab
Gilooooo <118784503+Gilooooo@users.noreply.github.com>
2022-12-14 15:53:14 +0800

    Update login.jsx

--- components/Login/login.jsx
+++ components/Login/login.jsx
@@ -36,22 +36,18 @@ export default function LOGIN({LogiN, error}) {
 
 
     return (
-
-        
-        
-        <div className={styles.container}>
-        <div className={styles.image}>
-        <a href="http://localhost:3000/WEB">
-        <button>
-        <img src = "LOGO.png"></img>
-        </button>
-        </a>
-        </div>
+    <div className={styles.container}>
+            <div className={styles.image}>
+                <a href="http://localhost:3000/WEB">
+                <button>
+                    <img src = "LOGO.png"></img>
+                </button>
+                </a>
+            </div>
         <div className={styles.credit}>
-        <h1> </h1>
         <form onSubmit={submitHandler}>
             <div>
-                <h3> EMAIL / USERNAME</h3>
+                <h3>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; EMAIL / USERNAME</h3>
                 <input type="text" name="usernamE" onChange={e => setDetails({...details, usernamE: e.target.value})} value={details.usernamE}></input>
             </div>
             <div>
@@ -62,36 +58,34 @@ export default function LOGIN({LogiN, error}) {
                 <button type="submit" value="LOGIN"  onClick={Confirm} disabled={!details.usernamE||!password} > LOGIN </button>
             </div>     
         </form>    
-  
         </div>
-    
         <div className ={styles.TEXT}>
             <a href="http://localhost:3000/reg">
-            <button>CREATE ACCOUNT</button>
+                <button>CREATE ACCOUNT</button>
             </a>
-        <a href="http://localhost:3000/fpass"> 
-        <button>FORGOT PASSWORD</button>
-        </a>
+            <a href="http://localhost:3000/fpass"> 
+                <button>FORGOT PASSWORD</button>
+            </a>
         </div>
         <hr></hr>
         <div className={styles.google}>
-        <a href="https://myaccount.google.com/?utm_source=sign_in_no_continue&pli=1">
-        <button>
-        <img src="google.png"></img>
-        </button>
-        </a>
-        <a href="https://www.facebook.com">
-        <button>
-        <img src="facebook.png"></img>
-        </button>
-        </a>
-        <a href="https://appleid.apple.com/sign-in">
-        <button>
-        <img src="apple.png"></img>
-        </button>
-        </a>
+            <a href="https://myaccount.google.com/?utm_source=sign_in_no_continue&pli=1">
+                <button>
+                    <img src="google.png"></img>
+                </button>
+            </a>
+            <a href="https://www.facebook.com">
+                <button>
+                    <img src="facebook.png"></img>
+                </button>
+            </a>
+            <a href="https://appleid.apple.com/sign-in">
+                <button>
+                    <img src="apple.png"></img>
+                </button>
+            </a>
         </div>
-        </div>
+    </div>
     
 
     );
